948adec1075f23c2dd62d6fba86a13f9ef1698f4
Curtis Dean <cdean.atx@gmail.com>
2021-08-21 13:37:26 -0500

    3.5.4

--- PyPoll.py
+++ PyPoll.py
@@ -37,10 +37,14 @@ with open(f_read_name, encoding='UTF-8') as election_data:
         #tally vote counter for current candidate
         candidate_votes[candidate_current] += 1
 
-    #Calculate percentage of votes for each candidate
+#Calculate percentage of votes for each candidate
+for candidate in candidate_votes:
+    votes = candidate_votes[candidate]
+    vote_percentage = float(votes) / float(votes_count) * 100
+    print(f"{candidate} has {vote_percentage:.2f}% of total votes")
 
 
-print(f"{votes_count:,} total votes\nCandidates: " + str(candidates)[1:-1] + "\nCandidate votes: " + str(candidate_votes)[1:-1])
+#print(f"{votes_count:,} total votes\nCandidates: " + str(candidates)[1:-1] + "\nCandidate votes: " + str(candidate_votes)[1:-1])
 
 #Open a text file where I will write the results of my analysis
 with open(f_write_name, "w") as analysis_txt:
