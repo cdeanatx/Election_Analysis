42ee5b0673135ce1dff10ee69c58ba5c218a87db
Curtis Dean <cdean.atx@gmail.com>
2021-08-21 12:42:41 -0500

    3.5.1

--- PyPoll.py
+++ PyPoll.py
@@ -4,10 +4,14 @@
 #4) Calculate the percentage of votes each candidate received
 #5) Find the winner of the election based on popular vote
 
+#import modules and define file names
 import csv
 import os
 f_read_name = "Resources/election_results.csv"
 f_write_name = "Analysis/election_analysis.txt"
+
+#Initialize vars
+votes_count = 0
 
 #open the election results and read the file
 with open(f_read_name, encoding='UTF-8') as election_data:
@@ -15,13 +19,14 @@ with open(f_read_name, encoding='UTF-8') as election_data:
     #Read the file
     f_reader = csv.reader(election_data)
 
-    #Print the header row
+    #Get column headers
     f_headers = next(f_reader)
-    print(f_headers)
 
+    #count votes
     for row in f_reader:
-        #print(row)
-        None
+        votes_count += 1 #total vote accumulator
+
+print(f"{votes_count:,} total votes")
 
 #Open a text file where I will write the results of my analysis
 with open(f_write_name, "w") as analysis_txt:
