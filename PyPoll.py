108904673c9c701e8da33505868f281657d32f6a
Curtis Dean <cdean.atx@gmail.com>
2021-08-21 09:20:43 -0500

    Added files for analysis

---
+++ PyPoll.py
@@ -0,0 +1,9 @@
+#1) Find the total number of votes
+#2) Create a list of cadidates who received votes
+#3) Tally the votes each candidate recveived
+#4) Calculate the percentage of votes each candidate received
+#5) Find the winner of the election based on popular vote
+
+import csv
+
+dir(csv)
